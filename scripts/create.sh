52ee773360c88c9ba9813effc38618c05f148d57
TuringProblem <andrew.deca.wellington@gmail.com>
2025-01-08 00:46:05 -0500

    feat: script should now work - head to /scrips/create.sh to load gittua locally

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,18 +2,28 @@
 
 # Author: { @Override } | 1/7/2025 ; 19:14
 
-function create() {
-    echo "Running Script...\n"
-    ocaml gittua ./src/gittua/spitonthatthang/gittua.ml
-    echo "Created file!\n"
-    sudo mv ./src/gittua/spitonthatthang/gittua /usr/local/bin/
-    echo "Moved File!\n"
+function isInstalled() {
+    if ! command -V ocamlopt &> /dev/null
+    then
+        echo "Ocaml is not installed. Please click https://ocaml.org/docs/installing-ocaml\n"
+        exit 1
+    fi
 }
-create
-echo "File has been created! Would you like to run?"
-read answer
 
-case "$answer" in 
-    "y") chmod +x /usr/local/bin/gittua 
-    *) echo "get fucked :p"
-esac
+isInstalled
+DIR=$(dirname "$(realpath "$0")")
+cd "$DIR" || exit 1
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation failed.\n"
+    exit 1
+fi
+
+echo "Now Installing GitTUA globally..."
+sudo mv gittua /usr/local/bin/gittua
+
+if [ $? -ne 0 ]; then
+    echo "GitTUA has been successfully installed\nRun gittua to SPIT ON THAT THANG! :)"
+else
+    echo "Error installing GitTUA"
+    exit 1
+fi
