660dceaa9c77b74d87b3238958077426543c84cc
Override <122590935+TuringProblem@users.noreply.github.com>
2025-01-10 13:27:43 -0500

    Feat: Changed the python code, hopefully this is more readable

--- src/gittua/python/main/gittua.py
+++ src/gittua/python/main/gittua.py
@@ -1,12 +1,10 @@
 # @author { @Override } | 1/9/2025 : 0133
-ANSI = {
 
-}
 def getUsr():
     name = input("Enter your Github Profile: ")
     return name
-github = getUsr()
 
+github = getUsr()
 
 def gitTua(f,a):
     return f(a)
@@ -16,23 +14,25 @@ def gitBlank(str):
 
 def branch():
     return 0
-    
 
-def handler(key):
-    match key:
-        case "cl":
-            command = gitTua(gitBlank, "clone")
-            print(f"We worked :) see {command}")
-            return command
-        case _:
-            return 0
-
-    
+git =  lambda x : gitTua(gitBlank, x)
 
 commands = {
-        'cl': handler("cl"),
-        'b': handler("b")
+        'cl': git("clone"),
+        'b': git("branch"),
+        'f': git("fetch"),
+        'p': git("pull"),
+        'a': git("add"),
+        'fa': git("add ."),
+        'help': git("-help")
 }
 
-print("Hello")
+print("Testing:\n")
+
+print(commands['b'])
+print(commands['f'])
+print(commands['p'])
+print(commands['a'])
+print(commands['fa'])
+print(commands['help'])
 print(commands['cl'])
